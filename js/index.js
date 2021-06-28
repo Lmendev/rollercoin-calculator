959cf9354222d6819f7c1d103e5310ce933fd3b1
Luis Mendoza <lmendoza.mg@gmail.com>
2021-06-28 11:17:48 +0200

    coins to mine section added

--- js/index.js
+++ js/index.js
@@ -43,6 +43,10 @@ const unit = {
     "EH/s": 1000000000000000000
 }
 
+function calculateRewardPerBlock(networkPower, blockReward, userPower) {
+    return networkPower > 0? blockReward * userPower / networkPower : 0
+}
+
 function calculate () {
     let networkPowerSelected = document.getElementById("selectNetworkPower").value
     let inputNetworkPower = parseFloat(document.getElementById("inputNetworkPower").value || 0) * unit[networkPowerSelected]
@@ -54,7 +58,7 @@ function calculate () {
 
     let { timePerBlock } = blockReward[document.getElementById("selectBlockReward").value]
     
-    let rewardPerBlock = inputNetworkPower > 0? inputBlockReward * inputUserPower / inputNetworkPower : 0
+    let rewardPerBlock = calculateRewardPerBlock(inputNetworkPower, inputBlockReward, inputUserPower)
 
     let expextedReward  = rewardPerBlock.toFixed(LONG_FIXED)
     let dailyReward     = (rewardPerBlock * secondsInADay / timePerBlock).toFixed(SHORT_FIXED)
@@ -72,7 +76,7 @@ function calculate () {
 function handleCoinIcon () {
     let coin  = blockReward[document.getElementById("selectBlockReward").value]
 
-    document.getElementById("coinIcon").src = "./img/" + coin.icon
+    document.getElementById("coinIcon").src = "/img/" + coin.icon
     document.getElementById("coinIcon").alt = coin.name + " icon"
     document.getElementById("coinName").innerHTML = coin.name
     document.getElementById("coinTicker").innerHTML = coin.ticker
@@ -85,6 +89,58 @@ function handleBlockReward () {
     inputBlockReward.value = blockReward[blockRewardselected].dailyReward
     
     handleCoinIcon()
+}
+
+function calculateBestCoinsToMine(){
+    let btc = parseFloat(document.getElementById("inputNetworkPowerBTC").value || 0) * unit[document.getElementById("selectNetworkPowerBTC").value]
+    let doge = parseFloat(document.getElementById("inputNetworkPowerDOGE").value || 0) * unit[document.getElementById("selectNetworkPowerDOGE").value]
+    let eth = parseFloat(document.getElementById("inputNetworkPowerETH").value || 0) * unit[document.getElementById("selectNetworkPowerETH").value]
+    let rlt = parseFloat(document.getElementById("inputNetworkPowerRLT").value || 0) * unit[document.getElementById("selectNetworkPowerRLT").value]
+
+    let userPower = parseFloat(document.getElementById("inputUserPower").value || 0) * unit[document.getElementById("selectUserPower").value]
+
+    let networkPower = {btc, doge, eth, rlt}
+    const COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,dogecoin,ethereum&vs_currencies=usd"
+
+    fetch(COINGECKO_URL)
+    .then(res => res.json())
+    .then(coingecko => {
+        let newcoingecko = { ...coingecko, "rollertoken": { "usd": 1 }}
+        let result = []
+
+        for (ticker in blockReward) {
+            let rewardPerBlock = calculateRewardPerBlock(networkPower[ticker.toLowerCase()], parseFloat(blockReward[ticker].dailyReward || 0), userPower)
+            let { timePerBlock } = blockReward[ticker]
+
+            let monthlyRewardUSD   = (rewardPerBlock * secondsInAMonth / timePerBlock * newcoingecko[blockReward[ticker].name.toLowerCase()].usd).toFixed(SHORT_FIXED)
+
+            result.push({ 
+                ...blockReward[ticker],
+                monthlyRewardUSD 
+            })
+        }
+
+        let resultSorted = result.sort((a, b) => b.monthlyRewardUSD - a.monthlyRewardUSD)
+
+        let tbody = document.getElementById("result-table").getElementsByTagName("tbody")[0]
+        tbody.innerHTML = ""
+
+        for (coinResult in resultSorted){
+            tbody.innerHTML += 
+                `<tr>
+                    <td>
+                        <p>${resultSorted[coinResult].name}</p>
+                    </td>
+                    <td>
+                        <p>$${resultSorted[coinResult].monthlyRewardUSD} / Month</p>
+                    </td>
+                </tr>`
+        }
+
+
+        
+    })
+    .catch(err => { throw err });
 }
 
 handleBlockReward()
