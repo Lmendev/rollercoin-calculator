cf02b2262dafbbda729423fab413fbc21cce673e
Luis Mendoza <lmendoza.mg@gmail.com>
2022-03-14 12:13:56 +0100

    Update rollercoin.js

--- js/modules/rollercoin.js
+++ js/modules/rollercoin.js
@@ -14,14 +14,14 @@ export const blockReward = {
         icon: "doge.svg"
     },
     "ETH": {
-        dailyReward: 0.005,
+        dailyReward: 0.008,
         timePerBlock: 600,
         name: "Ethereum",
         ticker: "ETH",
         icon: "eth.svg"
     },
     "BNB": {
-        dailyReward: 0.012,
+        dailyReward: 0.02,
         timePerBlock: 600,
         name: "Binancecoin",
         ticker: "BNB",
