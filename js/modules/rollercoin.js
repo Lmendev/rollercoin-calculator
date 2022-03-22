d99827c07512065ec925f965d379833d156cdc14
Luis Mendoza <lmendoza.mg@gmail.com>
2022-03-22 10:33:00 +0100

    Update rollercoin.js

--- js/modules/rollercoin.js
+++ js/modules/rollercoin.js
@@ -1,41 +1,41 @@
 export const blockReward = {
     "BTC": {
-        dailyReward: 0.0003,
+        dailyReward: 0.00048,
         timePerBlock: 600,
         name: "Bitcoin",
         ticker: "BTC",
         icon: "btc.svg"
     },
     "DOGE": {
-        dailyReward: 20,
+        dailyReward: 53,
         timePerBlock: 600,
         name: "Dogecoin",
         ticker: "DOGE",
         icon: "doge.svg"
     },
     "ETH": {
-        dailyReward: 0.008,
+        dailyReward: 0.005,
         timePerBlock: 600,
         name: "Ethereum",
         ticker: "ETH",
         icon: "eth.svg"
     },
     "BNB": {
-        dailyReward: 0.02,
+        dailyReward: 0.012,
         timePerBlock: 600,
         name: "Binancecoin",
         ticker: "BNB",
         icon: "bnb.svg"
     },
     "MATIC": {
-        dailyReward: 4,
+        dailyReward: 3,
         timePerBlock: 600,
         name: "Polygon",
         ticker: "matic-network",
         icon: "matic.svg"
     },
     "RLT": {
-        dailyReward: 30,
+        dailyReward: 45,
         timePerBlock: 600,
         name: "Rollertoken",
         ticker: "RLT",
