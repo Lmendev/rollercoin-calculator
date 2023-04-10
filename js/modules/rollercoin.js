76dd87fba1ef4738b109fcaa2410bd059f4cc35d
Luis Mendoza <lmendoza.mg@gmail.com>
2023-04-10 18:58:53 -0400

    refactor(blockReward): ident using 2 spaces

--- js/modules/rollercoin.js
+++ js/modules/rollercoin.js
@@ -1,71 +1,71 @@
 export const blockReward = {
-    "BTC": {
-        dailyReward: 0.0004,
-        timePerBlock: 600,
-        name: "Bitcoin",
-        ticker: "BTC",
-        icon: "btc.svg"
-    },
-    "DOGE": {
-        dailyReward: 60,
-        timePerBlock: 600,
-        name: "Dogecoin",
-        ticker: "DOGE",
-        icon: "doge.svg"
-    },
-    "ETH": {
-        dailyReward: 0.007,
-        timePerBlock: 600,
-        name: "Ethereum",
-        ticker: "ETH",
-        icon: "eth.svg"
-    },
-    "BNB": {
-        dailyReward: 0.044,
-        timePerBlock: 600,
-        name: "Binancecoin",
-        ticker: "BNB",
-        icon: "bnb.svg"
-    },
-    "MATIC": {
-        dailyReward: 5.5,
-        timePerBlock: 600,
-        name: "Polygon",
-        ticker: "matic-network",
-        icon: "matic.svg"
-    },
-    "RLT": {
-        dailyReward: 20,
-        timePerBlock: 600,
-        name: "Rollertoken",
-        ticker: "RLT",
-        icon: "rlt.svg"
-    },
-    "SOL": {
-        dailyReward: 0.23,
-        timePerBlock: 600,
-        name: "Solana",
-        ticker: "SOL",
-        icon: "sol.svg"
-    },
-    "RST": {
-        dailyReward: 150,
-        timePerBlock: 600,
-        name: "Roller Season Token",
-        icon: "rst.svg"
-    },
-    "TRX": {
-        dailyReward: 60,
-        timePerBlock: 600,
-        name: "Tron",
-        ticker: "tron",
-        icon: "trx.svg"
-    },
-    "LTC": {
-        dailyReward: 0.015,
-        timePerBlock: 600,
-        name: "Litecoin",
-        ticker: "ltc",
-        icon: "ltc.svg"
-    },
+  "BTC": {
+    dailyReward: 0.0004,
+    timePerBlock: 600,
+    name: "Bitcoin",
+    ticker: "BTC",
+    icon: "btc.svg"
+  },
+  "DOGE": {
+    dailyReward: 60,
+    timePerBlock: 600,
+    name: "Dogecoin",
+    ticker: "DOGE",
+    icon: "doge.svg"
+  },
+  "ETH": {
+    dailyReward: 0.007,
+    timePerBlock: 600,
+    name: "Ethereum",
+    ticker: "ETH",
+    icon: "eth.svg"
+  },
+  "BNB": {
+    dailyReward: 0.044,
+    timePerBlock: 600,
+    name: "Binancecoin",
+    ticker: "BNB",
+    icon: "bnb.svg"
+  },
+  "MATIC": {
+    dailyReward: 5.5,
+    timePerBlock: 600,
+    name: "Polygon",
+    ticker: "matic-network",
+    icon: "matic.svg"
+  },
+  "RLT": {
+    dailyReward: 20,
+    timePerBlock: 600,
+    name: "Rollertoken",
+    ticker: "RLT",
+    icon: "rlt.svg"
+  },
+  "SOL": {
+    dailyReward: 0.23,
+    timePerBlock: 600,
+    name: "Solana",
+    ticker: "SOL",
+    icon: "sol.svg"
+  },
+  "RST": {
+    dailyReward: 150,
+    timePerBlock: 600,
+    name: "Roller Season Token",
+    icon: "rst.svg"
+  },
+  "TRX": {
+    dailyReward: 60,
+    timePerBlock: 600,
+    name: "Tron",
+    ticker: "tron",
+    icon: "trx.svg"
+  },
+  "LTC": {
+    dailyReward: 0.015,
+    timePerBlock: 600,
+    name: "Litecoin",
+    ticker: "ltc",
+    icon: "ltc.svg"
+  },
 }
